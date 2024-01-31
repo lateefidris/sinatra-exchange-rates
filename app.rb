f77795cb3456590c2216dc2494993f015a914678
Lateef Idris <150227412+lateefidris@users.noreply.github.com>
2024-01-31 02:17:07 +0000

    Added Currency Converter

--- app.rb
+++ app.rb
@@ -26,7 +26,14 @@ raw_currencies =  parsed_api.fetch("currencies")
 currencies = raw_currencies.values
 currency_abbr = raw_currencies.keys
 
-pp currency_abbr
 
 # Currency Converter
+currency_to = "USD"
+currency_from = "INR"
+amount = "4"
+convert_url = "http://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{currency_from}&to=#{currency_to}&amount=#{amount}"
 
+raw_convert_api = HTTP.get(convert_url)
+parsed_convert_api = JSON.parse(raw_convert_api)
+
+pp parsed_convert_api.fetch("result")
