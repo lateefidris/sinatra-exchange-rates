40f33ace09e657ac94dd3caf5087cfaa68990790
Lateef Idris <150227412+lateefidris@users.noreply.github.com>
2024-01-31 01:40:34 +0000

    Added Available Currencies To Array

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
+=begin
 require "sinatra"
 require "sinatra/reloader"
+
+api_url = "http://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+
 
 get("/") do
   "
@@ -7,3 +11,22 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+=end
+require "http"
+require "json"
+
+# Available Currencies
+api_url = "http://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+
+raw_api = HTTP.get(api_url)
+parsed_api = JSON.parse(raw_api)
+raw_currencies =  parsed_api.fetch("currencies")
+
+currencies = raw_currencies.values
+currency_abbr = raw_currencies.keys
+
+pp currency_abbr
+
+# Currency Converter
+
